0068ff519a77ea36eee66fd4d22deb4c98490462
megadl <megamind_w@163.com>
2020-06-30 16:34:41 +0800

    4sum

---
+++ exercises/4sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSum(self, nums, target):
+        def findNsum(l, r, target, N, result, results):
+            if r - l + 1 < N or N < 2 or target < nums[l] * N or target > nums[r] * N:  # early termination
+                return
+            if N == 2:  # two pointers solve sorted 2-sum problem
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:  # recursively reduce N
+                for i in range(l, r + 1):
+                    if i == l or (i > l and nums[i - 1] != nums[i]):
+                        print(f'Processing find{N-1}sum----result is now {result}---results is now {results}')
+                        findNsum(i + 1, r, target - nums[i], N - 1, result + [nums[i]], results)
+
+        nums.sort()
+        results = []
+        findNsum(0, len(nums) - 1, target, 4, [], results)
+        return results
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [-3, -2, -1, 0, 0, 1, 2, 3]
+    target = 0
+    print(solution.fourSum(nums, target))
